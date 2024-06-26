e975baea75468b1d9ba9d3634f8c7383d4025ca9
unknown <463003@itri.org.tw>
2024-06-26 16:27:25 +0800

    added itri config

--- pointcept/datasets/itri.py
+++ pointcept/datasets/itri.py
@@ -67,7 +67,7 @@ class ItriDataset(DefaultDataset):
         scan_data = np.load(data_path)
         
         coord = scan_data[:, :3].reshape(-1, 3)
-        strength = scan_data[:, 3].reshape([-1, 1])
+        strength = scan_data[:, 3].reshape([-1, 1]) / 255
         
         # If you don't have segment data, you can create a dummy segment
         segment = np.zeros(scan_data.shape[0], dtype=np.int32)
