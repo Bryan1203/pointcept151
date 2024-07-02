2e308dad08ec3c0a50fb34bc87717efb67a8af3d
Bryan1203 <bryanchang1203@hotmail.com>
2024-07-02 10:57:50 +0800

    added fine tune config

--- pointcept/datasets/semantic_kitti.py
+++ pointcept/datasets/semantic_kitti.py
@@ -38,9 +38,9 @@ class SemanticKITTIDataset(DefaultDataset):
 
     def get_data_list(self):
         split2seq = dict(
-            train=[0, 1, 2, 3, 4, 5, 6, 7, 9, 10],
-            val=[0,2,5,6,7,8,9,10],
-            test=[11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21],
+            train=[3,6,8,10],
+            val=[9],
+            test=[9,10],
         )
         if isinstance(self.split, str):
             seq_list = split2seq[self.split]
