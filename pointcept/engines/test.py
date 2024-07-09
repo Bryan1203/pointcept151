45bb9401c1bb73a6150f99c469b9b0cd2bcb090c
Bryan1203 <bryanchang1203@hotmail.com>
2024-07-09 08:42:55 +0800

    edited config file

--- pointcept/engines/test.py
+++ pointcept/engines/test.py
@@ -186,7 +186,7 @@ class SemSegTester(TesterBase):
                 pred = torch.zeros((segment.size, self.cfg.data.num_classes)).cuda()
                 features = torch.zeros((segment.size, 64)).cuda()  # Initialize feature tensor
                 for i in range(len(fragment_list)):
-                    fragment_batch_size = 2
+                    fragment_batch_size = 1
                     s_i, e_i = i * fragment_batch_size, min(
                         (i + 1) * fragment_batch_size, len(fragment_list)
                     )
