27ea9a7e10e73249bed62ba68d5739eb40a8d85d
Bryan1203 <bryanchang1203@hotmail.com>
2024-07-30 14:33:47 +0800

    updated test.py

--- pointcept/engines/test.py
+++ pointcept/engines/test.py
@@ -204,11 +204,14 @@ class SemSegTester(TesterBase):
                             batch_num=len(fragment_list),
                         )
                     )
+
+                #save the probability of the selected class
+                probs = pred.max(1)[0].data.cpu().numpy()
+
                 pred = pred.max(1)[1].data.cpu().numpy()
                 np.save(pred_save_path, pred)
 
-                #save the probability of the selected class
-                probs = pred.max(1).data.cpu().numpy()
+                
 
             if "origin_segment" in data_dict.keys():
                 assert "inverse" in data_dict.keys()
