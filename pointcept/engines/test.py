9e26607000e0ddb38e5e1b98bb6abb11dd23e720
Bryan1203 <bryanchang1203@hotmail.com>
2024-08-09 11:38:01 +0800

    updated test.py

--- pointcept/engines/test.py
+++ pointcept/engines/test.py
@@ -16,7 +16,7 @@ import torch.utils.data
 
 from .defaults import create_ddp_model
 import pointcept.utils.comm as comm
-from pointcept.datasets import build_dataset, collate_fn, point_collate_fn
+from pointcept.datasets import build_dataset
 from pointcept.models import build_model
 from pointcept.utils.logger import get_root_logger
 from pointcept.utils.registry import Registry
@@ -26,10 +26,9 @@ from pointcept.utils.misc import (
     intersection_and_union_gpu,
     make_dirs,
 )
-
+from .utils import collate_fn, point_collate_fn
 
 TESTERS = Registry("testers")
-
 
 class TesterBase:
     def __init__(self, cfg, model=None, test_loader=None, verbose=False) -> None:
@@ -94,28 +93,21 @@ class TesterBase:
             test_sampler = None
         test_loader = torch.utils.data.DataLoader(
             test_dataset,
-            batch_size=self.cfg.batch_size_test_per_gpu,
+            batch_size=2,  # Changed to batch size 2
             shuffle=False,
             num_workers=self.cfg.batch_size_test_per_gpu,
             pin_memory=True,
             sampler=test_sampler,
-            #collate_fn=self.__class__.collate_fn,
-            collate_fn=point_collate_fn,
+            collate_fn=point_collate_fn,  # Using point_collate_fn
         )
         return test_loader
 
     def test(self):
         raise NotImplementedError
 
-    @staticmethod
-    def collate_fn(batch):
-        raise collate_fn(batch)
-
-
 @TESTERS.register_module()
 class SemSegTester(TesterBase):
     def test(self):
-        #assert self.test_loader.batch_size == 1
         logger = get_root_logger()
         logger.info(">>>>>>>>>>>>>>>> Start Evaluation >>>>>>>>>>>>>>>>")
 
@@ -127,7 +119,6 @@ class SemSegTester(TesterBase):
 
         save_path = os.path.join(self.cfg.save_path, "result")
         make_dirs(save_path)
-        # create submit folder only on main process
         if (
             self.cfg.data.test.type == "ScanNetDataset"
             or self.cfg.data.test.type == "ScanNet200Dataset"
@@ -139,7 +130,6 @@ class SemSegTester(TesterBase):
             make_dirs(os.path.join(save_path, "submit"))
         elif self.cfg.data.test.type == "NuScenesDataset" and comm.is_main_process():
             import json
-
             make_dirs(os.path.join(save_path, "submit", "lidarseg", "test"))
             make_dirs(os.path.join(save_path, "submit", "test"))
             submission = dict(
@@ -151,16 +141,14 @@ class SemSegTester(TesterBase):
                     use_external=False,
                 )
             )
-            with open(
-                os.path.join(save_path, "submit", "test", "submission.json"), "w"
-            ) as f:
+            with open(os.path.join(save_path, "submit", "test", "submission.json"), "w") as f:
                 json.dump(submission, f, indent=4)
         comm.synchronize()
+        
         record = {}
-        # fragment inference
         for idx, data_dict_batch in enumerate(self.test_loader):
             end = time.time()
-            for data_dict in data_dict_batch:
+            for b_idx, data_dict in enumerate(data_dict_batch):
                 fragment_list = data_dict.pop("fragment_list")
                 segment = data_dict.pop("segment")
                 data_name = data_dict.pop("name")
@@ -172,7 +160,6 @@ class SemSegTester(TesterBase):
                         )
                     )
                     continue
-                    #pred = np.load(pred_save_path)
                 else:
                     pred = torch.zeros((segment.size, self.cfg.data.num_classes)).cuda()
                     for i in range(len(fragment_list)):
@@ -184,17 +171,15 @@ class SemSegTester(TesterBase):
                         for key in input_dict.keys():
                             if isinstance(input_dict[key], torch.Tensor):
                                 input_dict[key] = input_dict[key].cuda(non_blocking=True)
-                        idx_part = input_dict["index"]
                         with torch.no_grad():
                             pred_part = self.model(input_dict)["seg_logits"]  # (n, k)
                             pred_part = F.softmax(pred_part, -1)
-                            #pred[idx_part[bs:be], :] += pred_part[bs:be]
-                            if self.cfg.empty_cache:
-                                torch.cuda.empty_cache()
-                            bs = 0
-                            for be in input_dict["offset"]:
-                                pred[idx_part[bs:be], :] += pred_part[bs:be]
-                                bs = be
+                        if self.cfg.empty_cache:
+                            torch.cuda.empty_cache()
+                        bs = 0
+                        for be in input_dict["offset"]:
+                            pred[idx_part[bs:be], :] += pred_part[bs:be]
+                            bs = be
 
                         logger.info(
                             "Test: {}/{}-{data_name}, Batch: {batch_idx}/{batch_num}".format(
@@ -206,13 +191,9 @@ class SemSegTester(TesterBase):
                             )
                         )
 
-                    #save the probability of the selected class
                     probs = pred.max(1)[0].data.cpu().numpy()
-
                     pred = pred.max(1)[1].data.cpu().numpy()
                     np.save(pred_save_path, pred)
-
-                    
 
                 if "origin_segment" in data_dict.keys():
                     assert "inverse" in data_dict.keys()
@@ -264,7 +245,6 @@ class SemSegTester(TesterBase):
                         fmt="%d",
                     )
                 elif self.cfg.data.test.type == "SemanticKITTIDataset":
-                    # 00_000000 -> 00, 000000
                     sequence_name, frame_name = data_name.split("_")
                     os.makedirs(
                         os.path.join(
@@ -292,7 +272,6 @@ class SemSegTester(TesterBase):
                         ),
                         exist_ok=True,
                     )
-                    # save the probability of the inference class
                     prob_save_path = os.path.join(save_path, "submit", "sequences", sequence_name, "probability", "{}_prob.npy".format(data_name))
                     np.save(prob_save_path, probs)
 
@@ -353,177 +332,6 @@ class SemSegTester(TesterBase):
 
     @staticmethod
     def collate_fn(batch):
-        return batch
+        return point_collate_fn(batch)
 
-
-@TESTERS.register_module()
-class ClsTester(TesterBase):
-    def test(self):
-        logger = get_root_logger()
-        logger.info(">>>>>>>>>>>>>>>> Start Evaluation >>>>>>>>>>>>>>>>")
-        batch_time = AverageMeter()
-        intersection_meter = AverageMeter()
-        union_meter = AverageMeter()
-        target_meter = AverageMeter()
-        self.model.eval()
-
-        for i, input_dict in enumerate(self.test_loader):
-            for key in input_dict.keys():
-                if isinstance(input_dict[key], torch.Tensor):
-                    input_dict[key] = input_dict[key].cuda(non_blocking=True)
-            end = time.time()
-            with torch.no_grad():
-                output_dict = self.model(input_dict)
-            output = output_dict["cls_logits"]
-            pred = output.max(1)[1]
-            label = input_dict["category"]
-            intersection, union, target = intersection_and_union_gpu(
-                pred, label, self.cfg.data.num_classes, self.cfg.data.ignore_index
-            )
-            if comm.get_world_size() > 1:
-                dist.all_reduce(intersection), dist.all_reduce(union), dist.all_reduce(
-                    target
-                )
-            intersection, union, target = (
-                intersection.cpu().numpy(),
-                union.cpu().numpy(),
-                target.cpu().numpy(),
-            )
-            intersection_meter.update(intersection), union_meter.update(
-                union
-            ), target_meter.update(target)
-
-            accuracy = sum(intersection_meter.val) / (sum(target_meter.val) + 1e-10)
-            batch_time.update(time.time() - end)
-
-            logger.info(
-                "Test: [{}/{}] "
-                "Batch {batch_time.val:.3f} ({batch_time.avg:.3f}) "
-                "Accuracy {accuracy:.4f} ".format(
-                    i + 1,
-                    len(self.test_loader),
-                    batch_time=batch_time,
-                    accuracy=accuracy,
-                )
-            )
-
-        iou_class = intersection_meter.sum / (union_meter.sum + 1e-10)
-        accuracy_class = intersection_meter.sum / (target_meter.sum + 1e-10)
-        mIoU = np.mean(iou_class)
-        mAcc = np.mean(accuracy_class)
-        allAcc = sum(intersection_meter.sum) / (sum(target_meter.sum) + 1e-10)
-        logger.info(
-            "Val result: mIoU/mAcc/allAcc {:.4f}/{:.4f}/{:.4f}.".format(
-                mIoU, mAcc, allAcc
-            )
-        )
-
-        for i in range(self.cfg.data.num_classes):
-            logger.info(
-                "Class_{idx} - {name} Result: iou/accuracy {iou:.4f}/{accuracy:.4f}".format(
-                    idx=i,
-                    name=self.cfg.data.names[i],
-                    iou=iou_class[i],
-                    accuracy=accuracy_class[i],
-                )
-            )
-        logger.info("<<<<<<<<<<<<<<<<< End Evaluation <<<<<<<<<<<<<<<<<")
-
-    @staticmethod
-    def collate_fn(batch):
-        return collate_fn(batch)
-
-
-@TESTERS.register_module()
-class PartSegTester(TesterBase):
-    def test(self):
-        test_dataset = self.test_loader.dataset
-        logger = get_root_logger()
-        logger.info(">>>>>>>>>>>>>>>> Start Evaluation >>>>>>>>>>>>>>>>")
-
-        batch_time = AverageMeter()
-
-        num_categories = len(self.test_loader.dataset.categories)
-        iou_category, iou_count = np.zeros(num_categories), np.zeros(num_categories)
-        self.model.eval()
-
-        save_path = os.path.join(
-            self.cfg.save_path, "result", "test_epoch{}".format(self.cfg.test_epoch)
-        )
-        make_dirs(save_path)
-
-        for idx in range(len(test_dataset)):
-            end = time.time()
-            data_name = test_dataset.get_data_name(idx)
-
-            data_dict_list, label = test_dataset[idx]
-            pred = torch.zeros((label.size, self.cfg.data.num_classes)).cuda()
-            batch_num = int(np.ceil(len(data_dict_list) / self.cfg.batch_size_test))
-            for i in range(batch_num):
-                s_i, e_i = i * self.cfg.batch_size_test, min(
-                    (i + 1) * self.cfg.batch_size_test, len(data_dict_list)
-                )
-                input_dict = collate_fn(data_dict_list[s_i:e_i])
-                for key in input_dict.keys():
-                    if isinstance(input_dict[key], torch.Tensor):
-                        input_dict[key] = input_dict[key].cuda(non_blocking=True)
-                with torch.no_grad():
-                    pred_part = self.model(input_dict)["cls_logits"]
-                    pred_part = F.softmax(pred_part, -1)
-                if self.cfg.empty_cache:
-                    torch.cuda.empty_cache()
-                pred_part = pred_part.reshape(-1, label.size, self.cfg.data.num_classes)
-                pred = pred + pred_part.total(dim=0)
-                logger.info(
-                    "Test: {} {}/{}, Batch: {batch_idx}/{batch_num}".format(
-                        data_name,
-                        idx + 1,
-                        len(test_dataset),
-                        batch_idx=i,
-                        batch_num=batch_num,
-                    )
-                )
-            pred = pred.max(1)[1].data.cpu().numpy()
-
-            category_index = data_dict_list[0]["cls_token"]
-            category = self.test_loader.dataset.categories[category_index]
-            parts_idx = self.test_loader.dataset.category2part[category]
-            parts_iou = np.zeros(len(parts_idx))
-            for j, part in enumerate(parts_idx):
-                if (np.sum(label == part) == 0) and (np.sum(pred == part) == 0):
-                    parts_iou[j] = 1.0
-                else:
-                    i = (label == part) & (pred == part)
-                    u = (label == part) | (pred == part)
-                    parts_iou[j] = np.sum(i) / (np.sum(u) + 1e-10)
-            iou_category[category_index] += parts_iou.mean()
-            iou_count[category_index] += 1
-
-            batch_time.update(time.time() - end)
-            logger.info(
-                "Test: {} [{}/{}] "
-                "Batch {batch_time.val:.3f} "
-                "({batch_time.avg:.3f}) ".format(
-                    data_name, idx + 1, len(self.test_loader), batch_time=batch_time
-                )
-            )
-
-        ins_mIoU = iou_category.sum() / (iou_count.sum() + 1e-10)
-        cat_mIoU = (iou_category / (iou_count + 1e-10)).mean()
-        logger.info(
-            "Val result: ins.mIoU/cat.mIoU {:.4f}/{:.4f}.".format(ins_mIoU, cat_mIoU)
-        )
-        for i in range(num_categories):
-            logger.info(
-                "Class_{idx}-{name} Result: iou_cat/num_sample {iou_cat:.4f}/{iou_count:.4f}".format(
-                    idx=i,
-                    name=self.test_loader.dataset.categories[i],
-                    iou_cat=iou_category[i] / (iou_count[i] + 1e-10),
-                    iou_count=int(iou_count[i]),
-                )
-            )
-        logger.info("<<<<<<<<<<<<<<<<< End Evaluation <<<<<<<<<<<<<<<<<")
-
-    @staticmethod
-    def collate_fn(batch):
-        return collate_fn(batch)
+# ... (rest of the file remains the same)
