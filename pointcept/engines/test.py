e94f1636511ec2e4aa88bbb34dc73e0aa5071c1d
Bryan1203 <bryanchang1203@hotmail.com>
2024-08-09 11:49:38 +0800

    updated test.py

--- pointcept/engines/test.py
+++ pointcept/engines/test.py
@@ -98,7 +98,7 @@ class TesterBase:
             num_workers=self.cfg.batch_size_test_per_gpu,
             pin_memory=True,
             sampler=test_sampler,
-            collate_fn=collate_fn,  # Using point_collate_fn
+            collate_fn=self.__class__.collate_fn,  # Using point_collate_fn
         )
         return test_loader
 
